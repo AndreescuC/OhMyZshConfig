557a2b90200c75d13695ade3f2cdb6581c755bd9
AndreescuC <andreescuconstantin.ac95@gmail.com>
2020-06-24 11:59:04 +0300

    Try catching the oh-my-zsh install

--- setup_config.sh
+++ setup_config.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+{
+	# Install oh-my-zsh
+	echo "===> Installing oh-my-zsh"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+} || {
+	echo "===> Something went wrong when installing oh-my-zsh!"
+}
 
 # Use custom theme
+echo "===> Setting theme"
 printf '\nZSH_THEME="nebirhos"\n' >> ~/.zshrc
 
 # Use my aliases
+echo "===> Creating aliases"
 mkdir ~/.zsh
 printf '\nalias ..="cd .."\nalias ...="cd ../.."\nalias ....="cd ../../.."\nalias .....="cd ../../../.."\n\nalias grep="nocorrect grep --color=auto"\n\nalias ll="ls -lh"\nalias run-help=man\nalias which-command=whence\n\nalias mark="pwd > ~/.sd"\nalias port="cd $(cat ~/.sd)"\nalias ll="ls -alF"' > ~/.zsh/aliasrc 
-
 printf '\nsource ~/.zsh/aliasrc\n' >> ~/.zshrc
